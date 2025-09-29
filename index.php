a218e0486310439196063d3f5a923cd699b3ce3e
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2025-09-30 01:41:54 +0200

    Database applied

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
 
+$form = true;
 $subjecte = $email = $missatge = $error = "";
 
-function comprovarDades($subjecte, $email, $missatge , &$error) {
-    if (empty($subjecte)) $error .= "El camp subjecte està buit.<br>";
-    if (empty($email)) $error .= "El camp email està buit.<br>";
-    else if (!preg_match("/^[A-Za-z0-9.-_%+]+@[A-Za-z0-9._]+\.[A-Za-z]+$/", $email)) $error .= "El camp email no és vàlid.<br>";
-    if (empty($missatge)) $error .= "El camp missatge està buit.<br>";
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
     $subjecte = $_POST['subjecte'] ?? "";
     $email = $_POST['email'] ?? "";
     $missatge = $_POST['message'] ?? "";
@@ -25,10 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $error = "El correu s'ha enviat correctament.<br>";
                 require './model/model.php';
                 $sender = require './config.php';
-                if (insertHistory($sender["SMTP_USER"], $email, $subjecte, $missatge)){
-                    $error .= "Historial actualitzat correctament.";
-                    //TODO: Crear objeto de tipo mail y guardarlo para poder mostrarlo despues en una tabla
-                }
+                if (insertHistory($sender["SMTP_USER"], $email, $subjecte, $missatge)) $error .= "Historial actualitzat correctament.";
                 else $error .= "No s'ha pogut actualitzar l'historial. Error amb la base de dades.";
             }
             $subjecte = $email = $missatge = "";
@@ -39,6 +30,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-require_once "./view/view.php";
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['history_open'])){
+    $form = false;
+    require './model/model.php';
+    $history = getHistory();
+    require "./view/history.php";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['history_close'])) $form = true;
+
+function comprovarDades($subjecte, $email, $missatge , &$error) {
+    if (empty($subjecte)) $error .= "El camp subjecte està buit.<br>";
+    if (empty($email)) $error .= "El camp email està buit.<br>";
+    else if (!preg_match("/^[A-Za-z0-9.-_%+]+@[A-Za-z0-9._]+\.[A-Za-z]+$/", $email)) $error .= "El camp email no és vàlid.<br>";
+    if (empty($missatge)) $error .= "El camp missatge està buit.<br>";
+}
+
+if ($form) require_once "./view/view.php";
 
 ?>
--- model/model.php
+++ model/model.php
@@ -2,21 +2,10 @@
 
 function insertHistory($sender, $recipient, $subject, $message) {
     try {
-        // Ubicacio de la base de dades
-        $dbPath = __DIR__ . '/../historydb.db';
-        $pdo = new PDO("sqlite:" . $dbPath);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = createPDO();
         
-        // Crear la base de dades amb la taula si no existeix
-        $createTableSQL = "CREATE TABLE IF NOT EXISTS history (
-            history_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            sender TEXT NOT NULL,
-            recipient TEXT NOT NULL,
-            subject TEXT NOT NULL,
-            body TEXT NOT NULL
-        )";
-        $pdo->exec($createTableSQL);
-        
+        createHistoryTable($pdo);
+
         // Preparar i executar la inserció
         $stmt = $pdo->prepare("INSERT INTO history (sender, recipient, subject, body) VALUES (?, ?, ?, ?)");
         $stmt->execute([$sender, $recipient, $subject, $message]);
@@ -25,6 +14,45 @@ function insertHistory($sender, $recipient, $subject, $message) {
         
     } catch (PDOException $e) {
         return false;
+    }
+}
+
+function getHistory() {
+    try {
+        $pdo = createPDO();
+
+        createHistoryTable($pdo);
+        
+        // Preparar i executar la consulta
+        $stmt = $pdo->query("SELECT sender, recipient, subject, body FROM history");
+        $history = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        return $history;
+        
+    } catch (PDOException $e) {
+        return false;
+    }
+}
+
+function createPDO(): PDO {
+    $dbPath = __DIR__ . '/../historydb.db';
+    $pdo = new PDO("sqlite:" . $dbPath);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+}
+
+function createHistoryTable($pdo) {
+    try {
+        $createTableSQL = "CREATE TABLE IF NOT EXISTS history (
+            history_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sender TEXT NOT NULL,
+            recipient TEXT NOT NULL,
+            subject TEXT NOT NULL,
+            body TEXT NOT NULL
+        )";
+        $pdo->exec($createTableSQL);
+    } catch (PDOException $e) {
+        throw new Exception("No se ha podido crear la base de datos.");
     }
 }
 
