79b7583ef9e557392b360178cf0dc74be8161f91
Eric Mejias Gamonal <ericmejiasgamonal@gmail.com>
2025-09-29 19:25:40 +0200

    fusion index.php con controller.php

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
 <?php
 
+$subjecte = $email = $missatge = $error = "";
+
+function comprovarDades($subjecte, $email, $missatge , &$error) {
+    if (empty($subjecte)) $error .= "El camp subjecte està buit.<br>";
+    if (empty($email)) $error .= "El camp email està buit.<br>";
+    else if (!preg_match("/^[A-Za-z0-9.-_%+]+@[A-Za-z0-9._]+\.[A-Za-z]+$/", $email)) $error .= "El camp email no és vàlid.<br>";
+    if (empty($missatge)) $error .= "El camp missatge està buit.<br>";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $subjecte = $_POST['subjecte'] ?? "";
+    $email = $_POST['email'] ?? "";
+    $missatge = $_POST['message'] ?? "";
+    $error = "";
+
+    comprovarDades($subjecte, $email, $missatge, $error);
+
+    try {
+        if (empty($error)) {
+            require './controller/mailer.php';
+            if (!enviarCorreu($subjecte, $email, $missatge)) $error = "No s'ha pogut enviar el correu.";
+            else{
+                $error = "El correu s'ha enviat correctament.<br>";
+                //require './model/model.php';
+                //$sender = require './config.php';
+                //$error .= insertHistory($sender["SMTP_USER"], $email, $subjecte, $missatge);
+            }
+            $subjecte = $email = $missatge = "";
+        }
+    }
+    catch (Exception $e) {
+        $error = $e->getMessage();
+    }
+}
+
 require_once "./view/view.php";
 
 ?>
--- view/view.php
+++ view/view.php
@@ -1,7 +1,3 @@
-<?php
-require_once "controller/controller.php";
-?>
-
 <!DOCTYPE html>
 <html lang="ca">
 <head>
